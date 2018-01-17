434ef6abe977ef572d1c400b7f3957142e4d9c99
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2018-01-17 11:08:03 +0800

    Update config/boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,15 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require "bootsnap/setup"
+require 'bootsnap'
+env = ENV['RAILS_ENV'] || 'development'
+circle_off = ENV['CIRCLECI'].nil?
+Bootsnap.setup(
+  cache_dir: 'tmp/cache',
+  development_mode: env == 'development',
+  load_path_cache: circle_off,
+  autoload_paths_cache: circle_off,
+  disable_trace: true,
+  compile_cache_iseq: true,
+  compile_cache_yaml: true
+)
