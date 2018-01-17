327dca0892e09ca5670278eff21d70a765009fcc
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2018-01-17 10:44:12 +0800

    Add seed for user and category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.new(fname: "admin", lname: "admin", mobile: "1232321", email: "admin@admin.com", password: "admin123", birthdate: Time.zone.today)
+user.save(validate: false)
+Category.create(name: "T-shirt")
+Category.create(name: "Bags")
+Category.create(name: "Hats")
