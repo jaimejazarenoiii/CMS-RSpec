6a1320327dff8df82e0827aa6b4bf3bb83ce579a
Indy Lejano <indylejano@yahoo.com>
2018-01-25 13:56:01 +0800

    Added RSpec Controllers

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,4 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
+  let(:user) {
+    User.create(
+      fname: "foo",
+      lname: "bar",
+      mobile: 123_456_789,
+      email: "sample@gmail.com",
+      birthdate: "1998-03-02",
+      role: "admin",
+      password: "foobar",
+      avatar:
+      Rack::Test::UploadedFile.new(
+        Rails.root.join('spec', 'images', '2.png')
+      )
+    )
+  }
+
+  let(:category) {
+    Category.create(
+      name: "foo"
+    )
+  }
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+  describe "GET#new" do
+    it "assigns new category to @category" do
+      get :new
+      expect(assigns(:category)).to be_a_new(Category)
+    end
+  end
+  describe "POST #create" do
+    it "creates a new Category" do
+      expect {
+        post :create, { params: { category: { name: "Bag" } } }
+      }.to change(Category, :count).by(1)
+    end
+    it "assigns created category to @category to be saved" do
+      category = Category.create!(name: "Bag")
+      expect(category).to be_a(Category)
+      expect(category).to eq(Category.last)
+    end
+    it "should not create category with nil value" do
+      category = Category.new(name: nil)
+      expect(category).to_not be_valid
+    end
+  end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,4 +1,99 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
+  let(:user) {
+    User.create(
+      fname: "foo",
+      lname: "bar",
+      mobile: 123_456_789,
+      email: "sample@gmail.com",
+      birthdate: "1998-03-02",
+      role: "admin",
+      password: "foobar",
+      avatar:
+      Rack::Test::UploadedFile.new(
+        Rails.root.join('spec', 'images', '2.png')
+      )
+    )
+  }
+
+  let(:category) {
+    Category.create!(
+      name: "foo"
+    )
+  }
+
+  let(:product) {
+    Product.create(
+      user_id: user.id,
+      name: "Cap",
+      description: "Black",
+      price: 90.75,
+      category_id: category.id,
+      image: Rack::Test::UploadedFile.new(
+        Rails.root.join('spec', 'images', '2.png')
+      )
+    )
+  }
+  let(:invalid_product) {
+    Product.create(
+      user_id: nil,
+      name: nil,
+      description: nil,
+      price: nil,
+      category_id: nil,
+      image: nil
+    )
+  }
+
+  before(:each) do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    sign_in user
+  end
+
+  describe "GET #index" do
+    it "assigns all products to @product" do
+      products = Product.all
+      get :index
+      expect(assigns(:products).map { |e| e }).to eq(products.map { |e| e })
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested product as @product" do
+      product
+      get :show, params: { id: product }
+      expect(assigns(:product)).to eq(product)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid parameters" do
+      it "creates a new product" do
+        product
+        expect(product).to be_valid
+      end
+    end
+
+    context "with invalid parameters" do
+      it "assigns a newly created but unsaved product as @product" do
+        invalid_product
+        expect(:invalid_product).not_to be_a(Product)
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "deletes the chosen product" do
+      product
+      expect {
+        delete :destroy, params: { id: product }
+      }.to change(Product, :count).by(-1)
+    end
+    it "redirects to product list" do
+      product
+      delete :destroy, params: { id: product }
+      expect(response).to redirect_to(products_url)
+    end
+  end
 end
